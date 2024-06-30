9a953c0645ecc14a8b687c402ba1870e834d5709
janewise <130130074+janewise@users.noreply.github.com>
2024-06-30 23:01:33 +0630

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,29 @@
-// src/firebase.js
-import firebase from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
-import 'firebase/database'; //'firebase/firestore' or 'firebase/database' if using Realtime Database
+// // src/firebase.js
+// import firebase from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+// import 'firebase/database'; //'firebase/firestore' or 'firebase/database' if using Realtime Database
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBb0STZdIj74Esu1kuuoDWlBsvCy-z1Bk4",
+//     authDomain: "bitbrawl-404.firebaseapp.com",
+//     databaseURL: "https://bitbrawl-404-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "bitbrawl-404",
+//     storageBucket: "bitbrawl-404.appspot.com",
+//     messagingSenderId: "403464469352",
+//     appId: "1:403464469352:web:ad6b3e3f44ab2da96374dd",
+//     measurementId: "G-PTE8NYVB75"
+//   };
+
+// // Initialize Firebase
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// // Initialize Firestore or Realtime Database
+// const db = firebaseApp.database(); //firebaseApp.firestore(); or firebaseApp.database();
+
+// export { db };
+import firebase from 'firebase/compat/app'; // Importing from 'firebase/compat/app' for Firebase v9+
+import { getAnalytics } from 'firebase/analytics';
+import 'firebase/compat/database'; // Importing from 'firebase/compat/database' for Firebase v9+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBb0STZdIj74Esu1kuuoDWlBsvCy-z1Bk4",
@@ -16,8 +38,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(firebaseApp);
+
 // Initialize Firestore or Realtime Database
-const db = firebaseApp.database(); //firebaseApp.firestore(); or firebaseApp.database();
+const db = firebaseApp.database(); // Use firebaseApp.firestore() for Firestore
 
 export { db };
+
